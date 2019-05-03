a3832f8a5ee87072c2b9988bc7ccb86e8ae4970e
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-05-04 03:38:22 +0900

    feat: add test for save

--- tests/CsProjEditor.Tests/SaveTest.cs
+++ tests/CsProjEditor.Tests/SaveTest.cs
@@ -47,12 +47,46 @@ namespace CsProjEditor.Tests
             Directory.Delete(tempFolder, true);
         }
 
-        [Fact]
-        public void SaveShouldbeTest()
+        [Theory]
+        [InlineData("hogemoge.csproj")]
+        public void SaveShouldbeTest(string path)
         {
             foreach (var csprojPath in tempPaths)
             {
                 var csproj = CsProjEditor.Load(csprojPath);
+                // Node / NodeValue
+                csproj.ExistsNode("PropertyGroup", "Hogemoge").Should().BeFalse();
+                csproj.InsertNode("PropertyGroup", "Hogemoge", "fugafuga");
+                csproj.ExistsNodeValue("PropertyGroup", "Hogemoge", "fugafuga").Should().BeTrue();
+
+                // Attribute / AttributeValue
+                csproj.ExistsNode("ItemGroup", "Hogemoge").Should().BeFalse();
+                csproj.InsertNode("ItemGroup", "Hogemoge", "fugafuga");
+                csproj.ExistsAttribute("ItemGroup", "Hogemoge", "Copy").Should().BeFalse();
+                csproj.SetAttribute("ItemGroup", "Hogemoge", "Copy", "hogemoge", e => !e.HasAttributes);
+                csproj.ExistsAttributeValue("ItemGroup", "Hogemoge", "Copy", "hogemoge").Should().BeTrue();
+
+                // save
+                var parent = Directory.GetParent(csprojPath);
+                var savePath = Path.Combine(parent.FullName, path);
+                csproj.Save(savePath);
+
+                // check save and ensure
+                var save = CsProjEditor.Load(savePath);
+                save.ExistsNodeValue("PropertyGroup", "Hogemoge", "fugafuga").Should().BeTrue();
+                save.ExistsNodeValue("ItemGroup", "Hogemoge", "fugafuga").Should().BeTrue();
+                save.ExistsAttributeValue("ItemGroup", "Hogemoge", "Copy", "hogemoge").Should().BeTrue();
+
+                // remove and ensure same as original
+                save.RemoveNode("PropertyGroup", "Hogemoge", false);
+                save.RemoveNode("ItemGroup", "Hogemoge", false);
+                save.ExistsNode("PropertyGroup", "Hogemoge").Should().BeFalse();
+                save.ExistsNode("ItemGroup", "Hogemoge").Should().BeFalse();
+                var comparePath = Path.Combine(parent.FullName, "compare.csproj");
+                save.Save(comparePath);
+                var compare = CsProjEditor.Load(comparePath);
+                // make sure comment line will be removed when Removenode with leaveBlankline = false
+                CsProjEditor.Load(csprojPath).ToString().Should().BeEquivalentTo(compare.ToString());
             }
         }
     }

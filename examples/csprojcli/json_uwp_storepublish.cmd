b6c21a99b71da4b44b9170de65b235bb8f8a904f
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-05-06 03:34:21 +0900

    feat: add package info

---
+++ examples/csprojcli/json_uwp_storepublish.cmd
@@ -0,0 +1,1 @@
+dotnet "..\..\src\csprojcli\bin\Debug\netcoreapp2.1\publish\csprojcli.dll" Batch.LoadAndRun -jsonPath uwp_storepublish.json
--- examples/dotnet-csproj/json_uwp_storepublish.cmd
+++
@@ -1,1 +0,0 @@
-dotnet "..\..\console\dotnet-csproj\bin\Debug\netcoreapp2.1\publish\dotnet-csproj.dll" Batch.LoadAndRun -jsonPath uwp_storepublish.json

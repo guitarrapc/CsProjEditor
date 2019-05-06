7444df242acc5fb248954667fafe369492c9cff3
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-05-06 18:02:44 +0900

    chore: clean up examples.
    [skip ci]

--- examples/csprojcli/commands.cmd
+++ examples/csprojcli/commands.cmd
@@ -1,5 +1,5 @@
-csprojcli NodeValue.Set -p SimpleNewCsProjUtf8_CRLF.csproj -g PropertyGroup -n PackageCertificateKeyFile -v hogehoge.pfx -dry false -output result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
-csprojcli Node.Insert -p result_SimpleNewCsProjUtf8_CRLF.csproj -g PropertyGroup -n PackageCertificateThumbprint -v 1234567890ABCDEF -dry false -output result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
-csprojcli Attribute.Insert -p result_SimpleNewCsProjUtf8_CRLF.csproj -g ItemGroup -n None -a Include -v hogehoge.pfx -dry false -output result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
-csprojcli Group.Insert -p result_SimpleNewCsProjUtf8_CRLF.csproj -g TestGroup -dry false -output result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
-csprojcli Node.Insert -p result_SimpleNewCsProjUtf8_CRLF.csproj -g TestGroup -n Foo -v Bar -dry false -output result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
+csprojcli NodeValue.Set -p testdata/SimpleNewCsProjUtf8_CRLF.csproj -g PropertyGroup -n PackageCertificateKeyFile -v hogehoge.pfx -dry false -output testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
+csprojcli Node.Insert -p testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -g PropertyGroup -n PackageCertificateThumbprint -v 1234567890ABCDEF -dry false -output testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
+csprojcli Attribute.Insert -p testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -g ItemGroup -n None -a Include -v hogehoge.pfx -dry false -output testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
+csprojcli Group.Insert -p testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -g TestGroup -dry false -output testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
+csprojcli Node.Insert -p testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -g TestGroup -n Foo -v Bar -dry false -output testdata/result_SimpleNewCsProjUtf8_CRLF.csproj -allowoverwrite true
